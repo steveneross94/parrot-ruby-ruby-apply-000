b9cc75b6a913d1995956535dac53e0c32c172de9
Steven Ross <ross.steven@me.com>
2020-02-20 01:10:30 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-  phrase = "Squawk!"
+  phrase = "Pretty Bird!"
   puts phrase
   return phrase
 end
